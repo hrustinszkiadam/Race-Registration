f00fb1249d33565978a1bbb5a1c8775ef27021a8
hrustinszkiadam <hrustinszkiadam@gmail.com>
2021-02-28 21:54:19 +0100

    Html--Request Handling

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.use(express.static("public"));
+
+const encodedParser = bodyParser.urlencoded({ extended: false });
+let racers = new Array;
+
+app.post("/register", encodedParser, (req, res) => {
+    const postReq = req.body;
+    racers.push({
+        name: postReq.firstName.toString() + " " + postReq.lastName.toString(),
+        email: postReq.email,
+        password: postReq.password,
+        age: postReq.age,
+        runLength: postReq.runLength
+    });
+    res.redirect("registered.html");
+});
+
+module.exports = racers;
 
 app.listen(3000);
